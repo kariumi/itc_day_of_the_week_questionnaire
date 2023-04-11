e3350c38b4a44f36bc66a36ff8fcc4e646a1b536
kariumi <kariumi1028a@gmail.com>
2023-04-11 09:01:43 +0900

    完成

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import csv
+import pprint
 
 # 【お知らせ】
 # 部門が減ったり増えたりしてもこのプログラムを使用することができます。部門名をdepartmentsリストに追加、削除してください。
@@ -7,7 +8,7 @@ import csv
 # 【デバッグ用】
 # csvファイルの説明(変更用)
 # 列の要素名
-# 2:部門、3:できない20時、4:できない21時、5:できない22時、6:質問、7:したい20時、8:したい21時、9:したい22時
+# 2:部門、3:できない20時、4:できない21時、5:できない22時、6:したい20時、7:したい21時、8:したい22時、9:質問
 #
 #
 
@@ -42,6 +43,9 @@ numOfMember = 0
 # 投票したメンバーの数(部門ごと)を記録
 numOfDepMenber = [0 for i in range(len(departments))]
 
+# 最もみんな参加できる日のminAを記録
+minAList = []
+
 with open("result.csv", encoding="utf-8") as f:
     reader = csv.reader(f)
     # csvファイルの中身を変数に格納しておく
@@ -63,7 +67,7 @@ with open("result.csv", encoding="utf-8") as f:
                             A[i][j][k] += 1
                 # 参加したい日を集計する
                 for j in range(3):  # j = 20,21,22時で回すためのもの
-                    shitai = member[7+j].split(", ")
+                    shitai = member[6+j].split(", ")
                     for k in range(len(dayOfWeek)):  # k = 曜日の数で回す
                         if dayOfWeek[k] in shitai:
                             B[i][j][k] += 1
@@ -79,6 +83,12 @@ with open("result.csv", encoding="utf-8") as f:
     # 集計終わり
 
 # 表示する文章を構成する。
+
+
+# 最も多くの部員が参加できる日を記録する
+dekinai = [[]for m in range(len(departments))]
+kouho = [[]for m in range(len(departments))]
+
 section = 1
 message = f"\n\
 【2023年度前期 曜日決めアンケート集計結果】\n\
@@ -99,18 +109,19 @@ for i in range(len(departments)):
     message += f"\
 【{departments[i]}】\n\n\
 ・投票総数：{numOfDepMenber[i]}\n\n"
-    minA = min(min(A[i], key=max))
+
+    minA = min(min(A[i], key=min))
+    minAList.append(minA)
     maxB = max(max(B[i], key=max))
     tmp = ""
 
     # 参加できない日を集める 20時：0~6,21時：7~13,22時：14~20
-    dekinai = []
     for j in range(len(A[i])):
         tmp += f"{time[j]}\t|"
         for k in range(len(A[i][j])):
             tmp += f"\t{A[i][j][k]}"
             if A[i][j][k] == minA:
-                dekinai.append(j*7+k)
+                dekinai[i].append([j*7+k, 0])
         tmp += f"\n    "
 
     # 参加できない日のアンケ結果を表示
@@ -156,38 +167,91 @@ for i in range(len(departments)):
                 shitai.append(j*7+k)
         tmp += f"\n    "
 
+    kouho = dekinai
+
+    # みんなが参加できる日の、参加したい人数を記録
+    for n in range(len(dekinai[i])):
+        youbi = dekinai[i][n][0] % 7
+        times = int(dekinai[i][n][0] / 7)
+        kouho[i][n][1] = B[i][times][youbi]
+
     # 参加したい日のアンケ結果を表示
 
     message += f"\
     ⭕参加したい日の投票結果を表示します。（最大値:{maxB}）\n\
     時間帯\t|\t月\t火\t水\t木\t金\t土\t日\n\
-    {tmp}\n\
-【{departments[i]}の総評】\n"
-
-    saiteki = []
-    if len(dekinai) == 1:
-        i = int(day/7)
-        j = day % 7
-        saiteki.append(f"【{dayOfWeek[j]}{time[i]}】\n")
-    else:
-        for day in dekinai:
-            if day in shitai:
-                i = int(day/7)
-                j = day % 7
-                saiteki.append(f"{dayOfWeek[j]}{time[i]}")
-    tmp = ""
-    if len(saiteki) == 1:  # 最もみんなが参加できる日が一つに絞られた場合
-        tmp = f"部会に最適な日が一つに絞られました。以下のとおりです。この日に部会に参加できない人は{minA}人、この日に参加したい人は{maxB}人です。\n"
-    else:
-        tmp += f"部会に最適な日が複数ありました。以下のとおりです。なお、この日に部会に参加できない人は{minA}人、この日に参加したい人は{maxB}人です。\n"
-
-    message += f"{tmp}\n⭐"
-    for day in saiteki:
-        message += f"{day}⭐、⭐"
-    message = message.rstrip("、⭐")
-    message += "⭐\n\n"
-
+    {tmp}\n"
     message += "----------------------------------------------------------------------------------------------------\n"
 
 
+# アンケ結果表示終了
+
+# アンケ結果から候補日を決定する処理↓
+# kouhoリストに全部入っている。
+
+# まずは参加したい人の数でリストをソート
+sorted_kouho = []
+for i in range(len(departments)):
+    sorted_kouho.append(sorted(kouho[i], key=lambda x: x[1], reverse=True))
+
+sorted_kouho2 = sorted_kouho
+message += f"⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐\n最も最良な候補日は、\n\n"
+maxBList = []
+
+for i in range(len(departments)):
+    # sorted_kouhoの参加したい人の最大値を出す
+    maxBList.append(sorted_kouho[i][0][1])
+
+    message += f"⭐ {departments[i]}⭐ (参加できない：{minAList[i]}人、参加したい{maxBList[i]}人)\n"
+
+    for j in sorted_kouho[i]:
+        if j[1] == maxBList[i]:
+            youbi = j[0] % 7
+            times = int(j[0]/7)
+            message += f"- {dayOfWeek[youbi]}{time[times]}\n"
+    message += f"\n"
+
+message += f"です。\n\n"
+message += f"上の候補が被っている場合は更に下の候補を参考にしてください。↓↓\n🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦\n"
+message += f"参加できない人が最も少ない候補をおすすめ順に並べると、(括弧内の数字は参加したい人の数)\n\n"
+
+for i in range(len(departments)):
+    message += f"🟦 {departments[i]}🟦 (参加できない：{minAList[i]}人)\n"
+    for j in sorted_kouho[i]:
+        youbi = j[0] % 7
+        times = int(j[0]/7)
+        message += f"- {dayOfWeek[youbi]}{time[times]}（{j[1]}）\n"
+
+    message += "\n"
+
+
+message += f"です。\n"
+message += f"ここから選択できない場合は、上のアンケ結果を参考に決めてください。\n"
+
+# saiteki = []
+# if len(dekinai) == 1:
+#     i = int(dekinai[0]/7)
+#     j = dekinai[0] % 7
+#     saiteki.append(f"【{dayOfWeek[j]}{time[i]}】\n")
+# else:
+#     for day in dekinai:
+#         if day in shitai:
+#             i = int(day/7)
+#             j = day % 7
+#             saiteki.append(f"{dayOfWeek[j]}{time[i]}")
+# tmp = ""
+# if len(saiteki) == 1:  # 最もみんなが参加できる日が一つに絞られた場合
+#     tmp = f"部会に最適な日が一つに絞られました。以下のとおりです。この日に部会に参加できない人は{minA}人、この日に参加したい人は{maxB}人です。\n"
+# else:
+#     tmp += f"部会に最適な日が複数ありました。以下のとおりです。なお、この日に部会に参加できない人は{minA}人、この日に参加したい人は{maxB}人です。\n"
+
+# message += f"{tmp}\n⭐"
+# for day in saiteki:
+#     message += f"{day}⭐、⭐"
+# message = message.rstrip("、⭐")
+# message += "⭐\n\n"
 print(message)
+
+
+with open("result.txt", mode="w", encoding='UTF-8') as f:
+    f.write(message)
